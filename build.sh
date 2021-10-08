9cd176c58af9272d10df9a9e8446bac6fb7cff57
Pierrick Bouvier <pierrick.bouvier@posteo.net>
2021-10-08 14:45:13 +0200

    copy google file at root

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ mkdir -p $out_folder
 
 sphinx_opt="-W -n -j auto" # warn, report missing ref, parallel
 sphinx-build $sphinx_opt -b html $script_dir/source $out_folder
+cp $script_dir/google5eebefa27159374c.html $out_folder/
 echo "file://$out_folder/index.html"
