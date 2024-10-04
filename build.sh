93298fe4be1b4337e3174dea7d2ed2f9622f4233
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-10-04 16:38:55 -0700

    copy robots + google file in docs

--- build.sh
+++ build.sh
@@ -4,8 +4,11 @@ set -euo pipefail
 
 script_dir=$(dirname $(readlink -f $0))
 out_folder="$script_dir/docs/"
+rm -rf $out_folder
 mkdir -p $out_folder
 
 sphinx_opt="-W -n -j auto" # warn, report missing ref, parallel
 sphinx-build $sphinx_opt -b html $script_dir/source $out_folder
+cp robots.txt google5eebefa27159374c.html $out_folder
+touch docs/.nojekyll
 echo "file://$out_folder/index.html"
