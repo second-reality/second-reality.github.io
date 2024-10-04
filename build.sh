2ba403f59cbd38686884ec526644f65b5087ea85
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-10-03 22:14:52 -0700

    remove useless cp

--- build.sh
+++ build.sh
@@ -8,5 +8,4 @@ mkdir -p $out_folder
 
 sphinx_opt="-W -n -j auto" # warn, report missing ref, parallel
 sphinx-build $sphinx_opt -b html $script_dir/source $out_folder
-cp $script_dir/google5eebefa27159374c.html $out_folder/
 echo "file://$out_folder/index.html"
