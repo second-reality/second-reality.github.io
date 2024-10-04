c957cfe5d68e27905e4453667582eebc3a9aca6c
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-10-03 22:14:24 -0700

    hide footer

--- source/conf.py
+++ source/conf.py
@@ -55,3 +55,10 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_show_sphinx = False
+
+html_theme_options = {
+  "footer_start": ["copyright"],
+  "footer_end": []
+}
