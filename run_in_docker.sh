41ddf370f46b06a2147c01b54715d911f6305b22
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2025-12-13 19:22:13 -0800

    use podman

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -12,20 +12,15 @@ die()
 
 scriptDir=$(dirname $(readlink -f $0))
 
-docker build -t pages - < $scriptDir/pages.Dockerfile
+podman build -t pages - < $scriptDir/pages.Dockerfile
 
 interactive="-it"
 tty > /dev/null || interactive=
 
 all_gid=$(id -G | tr ' ' '\n' | sed -e 's/^/--group-add /g' | tr '\n' ' ')
-docker run $interactive --rm=true \
+podman run $interactive --rm=true \
   -w $(pwd) \
   -v $(pwd):$(pwd) \
   -v $scriptDir:$scriptDir \
-  -u $UID:$(id -g) \
-  $all_gid \
-  -v /etc/passwd:/etc/passwd:ro \
-  -v /etc/group:/etc/group:ro \
-  -v /etc/shadow:/etc/shadow:ro \
   -v /tmp:/tmp \
   pages "$@"
